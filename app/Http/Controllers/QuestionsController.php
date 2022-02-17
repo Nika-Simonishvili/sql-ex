52c9a7c09e76545207194cdd1e6f8fdf651d2fad
Nika <simonishvili19@gmail.com>
2022-02-17 17:03:53 +0400

    updated controller

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -7,23 +7,28 @@ use App\Models\Question;
 
 class QuestionsController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        return Question::all();
+        return Question::all();  // get all questions
     }
 
-    public function show($id)
-    {
-        return Question::find($id);
-    }
-
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-
         $solution = $request->input('solution');
         $result = "App\Models\\" . $solution;
 
-        $data = eval("return $result");
+        $data = eval("return $result");   // evaluate solution as query and save given data
 
         return Question::create([
             'title' => $request->input('title'),
@@ -32,6 +37,24 @@ class QuestionsController extends Controller
         ]);
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Question::find($id);  // retrieve specific question with its id
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id)
     {
         $question = Question::findOrFail($id);
@@ -49,11 +72,17 @@ class QuestionsController extends Controller
         return $question;
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
         $question = Question::findOrFail($id);
         $question -> delete();
         return response(204);
     }
-
 }
+
